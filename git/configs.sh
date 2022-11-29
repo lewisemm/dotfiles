1c667c299e4a3143c416fe7910b9c8637449f7f4
Lewis M. Kabui <lewisemm@users.noreply.github.com>
2022-11-29 13:37:49 +0300

    Add signing key configurations - Configure desired gpg key to use with git. - Automatically sign any commits with gpg key.

--- git/configs.sh
+++ git/configs.sh
@@ -3,4 +3,5 @@
 git config --global user.email lewisemm@users.noreply.github.com
 git config --global user.name "Lewis M. Kabui"
 git config --global core.editor vi
-
+git config --global user.signingkey <signingkey>
+git config --global commit.gpgsign true
